856569cd94edc20c43c4d9977e93e1256288b4ee
tallyto <rodrigues.tallyto@hotmail.com>
2024-02-21 12:17:47 -0300

    feat: criando fila para envio de notificação

---
+++ src/main/java/com/tallyto/sicred/coopdecisionhub/config/queue/RabbitMQConfig.java
@@ -0,0 +1,31 @@
+package com.tallyto.sicred.coopdecisionhub.config.queue;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    private static final String EXCHANGE_NAME = "sessao-fechada-exchange";
+    private static final String QUEUE_NAME = "sessao-fechada-queue";
+    private static final String ROUTING_KEY = "sessao-fechada-rout-key";
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange directExchange) {
+        return BindingBuilder.bind(queue).to(directExchange).with(ROUTING_KEY);
+    }
+}
